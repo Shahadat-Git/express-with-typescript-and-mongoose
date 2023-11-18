2bf7417f90a5e80c032746ac3185fd2b6fbee09c
Shahadat-Git <noyon560@gmail.com>
2023-11-19 03:33:11 +0600

    [DONE] Create Student Feature

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { StudentRoutes } from './app/modules/student/student.route';
 
 const app: Application = express();
 
@@ -7,10 +8,21 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
+// application routes
+app.use('/api/v1/students', StudentRoutes);
+
 app.get('/', (req: Request, res: Response) => {
   const a = 10;
 
-  res.send(a);
+  res.send({ a });
+});
+
+// no route
+app.all('*', (req: Request, res: Response) => {
+  res.status(500).json({
+    status: false,
+    message: 'No Route Found',
+  });
 });
 
 export default app;
---
+++ src/app/modules/student/student.model.ts
@@ -0,0 +1,93 @@
+import { Schema, model } from 'mongoose';
+import {
+  Guardian,
+  LocalGuardian,
+  Student,
+  UserName,
+} from './student.interface';
+
+const userNameSchema = new Schema<UserName>({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  middleName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+});
+
+const guardianSchema = new Schema<Guardian>({
+  fatherName: {
+    type: String,
+  },
+  fatherOccupation: {
+    type: String,
+  },
+  fatherContactNo: {
+    type: String,
+  },
+  motherName: {
+    type: String,
+  },
+  motherContactNo: {
+    type: String,
+  },
+  motherOccupation: {
+    type: String,
+  },
+});
+
+const localGuardianSchema = new Schema<LocalGuardian>({
+  name: {
+    type: String,
+  },
+  occupation: {
+    type: String,
+  },
+  contactNo: {
+    type: String,
+  },
+  address: {
+    type: String,
+  },
+});
+
+const studentSchema = new Schema<Student>({
+  id: { type: String },
+  name: userNameSchema,
+  gender: ['male', 'female'],
+  dateOfBirth: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  contactNo: {
+    type: String,
+    required: true,
+  },
+  emergencyContactNo: {
+    type: String,
+    required: true,
+  },
+  bloodGroup: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+  presentAddress: {
+    type: String,
+  },
+  permanentAddress: {
+    type: String,
+  },
+  guardian: guardianSchema,
+  localGuardian: localGuardianSchema,
+  profileImg: {
+    type: String,
+  },
+  isActive: ['active', 'inactive'],
+});
+
+export const StudentModel = model<Student>('Student', studentSchema);
